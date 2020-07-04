60b0a5866e88a831ecf7f81a7c262f5acb1f871b
mokcheungyam <609356067@qq.com>
2020-07-04 14:29:37 +0800

    deep learning

--- code/c5.py
+++ code/c5.py
@@ -157,18 +157,307 @@ history = model.fit_generator(
 # 保存模型
 model.save('cats_and_dogs_small_1.h5')
 
+# 绘制训练过程中的损失曲线和精度曲线
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training loss')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# 利用ImageDataGenerator设置数据增强
+datagen = ImageDataGenerator(
+    rotation_range=40, 
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+    )
+
+
+# 显示几个随机增强后的训练图像
+from keras.preprocessing import image
+
+fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+
+img_path = fnames[3]
+
+img = image.load_img(img_path, target_size=(150, 150))
+
+x = image.img_to_array(img)
+
+x = x.reshape((1, ) + x.shape)
+
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
+
+# 定义一个包含dropout层的新卷积神经网络
+
+model = model.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compilt(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+
+# 利用数据增强生成器训练卷积神经网络
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+model.save('cats_and_dogs_small_2.h5')
+
+
+# 将VGG16卷积基实例化
+from keras.application improt VGG16
+
+# weigth指定模型初始化的权重检测点，include_top指定模型最后是否包含密集连接分类器，默认情况下会对应于1000个类别，
+# input_shape指定输入形状，不指定的话可以处理任意形状的输入
+conv_base = VGG16(weight='imagenet', include_top=False, input_shape=(150, 150, 3))
+
+
+# 1.不使用数据增强的快速特征提取
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = '/User/fchollet/Downloads/cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i*batch_size : (i + 1)*batch_size] = features_batch
+        labels[i*batch_size : (i + 1)*batch_size] = labels_batch
+        i += 1
+        if i*batch_size > = sample_count:
+            break
+    return features, labels
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+
+# 2.使用数据增强的特征提取
+from keras import models
+from keras import layers
+
+model = moedls.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+# 利用冻结的卷积基端到端地训练模型
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)  # 验证数据不能增强
+
+train_generator = train_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+
+# 冻结直到某一层的所有层
+conv_base.trainable = True
+set_trainable = False
+
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+# 微调模型
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-5), metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+# 使得曲线更平滑
+def smooth_curve(points, factor=0.8):
+    smoothed_point = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous*factor + point*(1-factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+plt.plot(epochs, smooth_curve(acc), 'bo', label='Smoothed training acc')
 
 
 
+# 预处理单张图像
+img_path = '/Users/fchollet/Downloads/cats_and_dogs_small/test/cats/cat.1700.jpg'
 
+from keras.preprocessing import image
+import numpy as np
 
+img = image.load_img(img_path, target_size=(150, 150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
 
+# 显示测试图像
+import matplotlib.pyplot as plt
 
+plt.imshow(img_tensor[0])
+plt.show()
 
+# 用一个输入张量和一个输出张量列表将模型实例化
+from keras import models
 
+layer_outputs = [layer.output for layer in model.layer[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 
+# 以预测模式运行模型
+activations = activation_model.predict(img_tensor)
 
+# 将第四个通道可视化
+import matplotlib.pyplot as plt
 
+plt.matshow(first_layer_activation[0,:, :, 4], cmap='viridis')
+
+# 将每个中间激活的所有通道可视化
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activations):
+    n_features = layer_activation.shape[-1]
+
+    size = layer_activation.shape[1]
+
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size,
+                         row * size : (row + 1) * size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1],
+        scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
 
 
 
