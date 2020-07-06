61b865f6dc34f5624a3da6a5921c556acf3330e2
mokcheungyam <609356067@qq.com>
2020-07-06 14:55:52 +0800

    deeplearning

--- code/c6.py
+++ code/c6.py
@@ -217,233 +217,124 @@ plt.show()
 
 
 
+# 简单RNN的Numpy实现
+import numpy as np
 
+timesteps = 100  # 输入序列的时间步数
+input_features = 32  # 输入特征空间的维度
+output_features = 64  # 输出特征空间的维度
 
+inputs = np.random.random((timesteps, input_features))
 
+state_t = np.zeros((output_features,))  # 初始状态，全零向量
 
+W = np.random.random((output_features, input_features))
+U = np.random.random((output_features, output_features))
+b = np.random.random((output_features,))
 
+successive_outputs = []
+for input_t in inputs:
+    output_t = np.tanh(np.dot(W, input_t) + np.dot(U, state_t) + b)
+    successive_outputs.append(output_t)
 
+    state_t = output_t
 
+final_output_sequence = np.stack(successive_outputs, axis=0)
 
+# 模型应用于IMDB电影评论分类
+# IMDB数据准备
+from keras.datasets import imdb
+from keras.preprocessing import sequence
 
+max_features = 10000  # 作为特征的单词个数
+maxlen = 500
+batch_size = 32
 
+print('Loading data...')
+(input_train, y_train), (input_test, y_test) = imdb.load_data(
+    num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
 
+print('Pad sequences (samples x time')
+input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
 
+# 使用Embedding和SimpleRNN
+from keras.layers import Dense
 
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
 
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
 
+# 绘制结果
+import matplotlib.pyplot as plt
 
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
+epochs = range(1, len(acc) + 1)
 
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
 
+plt.figure()
 
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation')
+plt.legend()
 
+plt.show()
 
 
 
+# 使用Keras中的LSTM层
+from keras.layers import LSTM
 
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
 
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
 
 
+# 数据标准化
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 生成时间序列样本及其目标的生成器
+def generator(data, looback, delay, min_index, max_index, shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows), lookback // step, data.shape[-1]))
+
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
